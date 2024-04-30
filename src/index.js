6b399fc07ea9c5d77133cffa79d2950785b62c53
João Oliveira <jaoovit@gmail.com>
2024-04-30 16:28:54 +0100

    display the API information in the html

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import "./style.css";
 
 // Set a location
 
-let address = "";
+let address = "Guimarães";
 
 const searchLocation = document.querySelector("#searchLocation");
 
 function setLocation() {
   address = searchLocation.value;
 }
+
+// display weather DOM
+
+const location = document.querySelector("#location");
+const image = document.querySelector("#image");
+const temperature = document.querySelector("#temperature");
+const condition = document.querySelector("#condition");
+const wind = document.querySelector("#wind");
+const humidity = document.querySelector("#humidity");
+const precipitation = document.querySelector("#precipitation");
 
 // Get weather
 
@@ -23,26 +33,22 @@ function getWeather(address) {
     })
     .then(function (response) {
       console.log(response);
+      location.textContent = `${response.location.name} / ${response.location.country}`;
+      image.src = response.current.condition.icon;
+      temperature.textContent = `${response.current.temp_c}ºC`;
+      condition.textContent = response.current.condition.text;
+      wind.textContent = `Wind: ${response.current.wind_kph} Km/h`;
+      humidity.textContent = `Humidity: ${response.current.humidity}`;
+      precipitation.textContent = `Precipitation: ${response.current.precip_mm} mm`;
     });
 }
-/*
-// display weather DOM
 
-const container = document.querySelector("#container");
+getWeather(address);
 
-function displayWeather() {
-  container.innerHTML = "";
-  const name = document.createElement("p");
-  name.setAttribute("id", "name");
-  container.appendChild(name);
-  name.textContent = weatherInfo.then;
-}
-*/
 // search button
 
 const searchBtn = document.querySelector("#searchBtn");
 searchBtn.addEventListener("click", () => {
   setLocation();
   getWeather(address);
-  //displayWeather();
 });
