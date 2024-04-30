64b076ff6a27f8908c9a58a461df6627707d4b24
João Oliveira <jaoovit@gmail.com>
2024-04-30 11:24:18 +0100

    create a function to get weather information from the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./style.css";
+
+// Get weather
+
+function getWeather(location) {
+  fetch(
+    `https://api.weatherapi.com/v1/current.json?key=84540330684148b78fe151345242704&q=${location}`,
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+}
